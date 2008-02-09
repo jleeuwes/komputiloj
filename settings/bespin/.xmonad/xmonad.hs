61ba9e7598f9b118f547eccaeab976fdf18219a8
jeroen <jeroen@37c0c1ce-2331-4627-86c0-9f85f175e55b>
2008-02-09 16:28:18 +0000

    Jottemdingen

--- settings/bespin/.xmonad/xmonad.hs
+++ settings/bespin/.xmonad/xmonad.hs
@@ -1,26 +1,31 @@
 import XMonad
 
+import XMonad.Layout.Tabbed
+import XMonad.Layout.WindowNavigation
+import XMonad.Hooks.DynamicLog
+
 import qualified XMonad.StackSet as W
 import Data.Map (union, fromList)
 
 
 
 
-main = xmonad defaults
+main = xmonad configuur
 
 
 
 
-defaults = defaultConfig {
+configuur = defaultConfig {
         terminal           = "gnome-terminal",
         borderWidth        = 2,
         workspaces         = ["com","werk","web","mail"],
         normalBorderColor  = "#c0c0c0",
         focusedBorderColor = "#000000",
-        defaultGaps        = [(0,0,0,0)],
+        defaultGaps        = [(15,0,0,0)],
         
         keys = (\c -> extraKeys c `union` keys defaultConfig c),
         
+        logHook = dynamicLog,
         layoutHook = layouts,
         manageHook = composeAll []
     }
@@ -28,7 +33,7 @@ defaults = defaultConfig {
 
 
 
-layouts = tiled ||| Mirror tiled ||| Full
+layouts = windowNavigation (tiled ||| Mirror tiled ||| tabbed shrinkText defaultTConf ||| Full)
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
@@ -47,23 +52,36 @@ layouts = tiled ||| Mirror tiled ||| Full
 
 extraKeys conf@(XConfig {XMonad.modMask = modMask}) = fromList [
 
-    -- Naar volgend venster
-    ((modMask, xK_Down), windows W.focusDown)
-
-    -- Naar vorig venster
-    , ((modMask, xK_Up), windows W.focusUp)
-
-    -- Verwissel met volgend venster
-    , ((modMask .|. shiftMask, xK_Down), windows W.swapDown)
-
-    -- Verwissel met vorig venster
-    , ((modMask .|. shiftMask, xK_Up), windows W.swapUp)
-
     -- Kleinere master
-    , ((modMask, xK_Left), sendMessage Shrink)
+    ((modMask, xK_minus), sendMessage Shrink)
 
     -- Grotere master
-    , ((modMask, xK_Right), sendMessage Expand)
+    , ((modMask, xK_equal), sendMessage Expand)
+
+    -- Naar volgend venster
+    , ((modMask, xK_Tab), windows W.focusUp)
+    
+    -- Naar vorig venster
+    , ((modMask .|. shiftMask, xK_Tab), windows W.focusUp)
+
+    -- Verwissel met volgend venster
+    , ((modMask .|. controlMask, xK_Tab), windows W.swapDown)
+
+    -- Verwissel met vorig venster
+    , ((modMask .|. shiftMask .|. controlMask, xK_Tab), windows W.swapUp)
+
+    -- Navigatie
+    , ((modMask,               xK_Right), sendMessage $ Go R)
+    , ((modMask,               xK_Left ), sendMessage $ Go L)
+    , ((modMask,               xK_Up   ), sendMessage $ Go U)
+    , ((modMask,               xK_Down ), sendMessage $ Go D)
+    , ((modMask .|. shiftMask, xK_Right), sendMessage $ Swap R)
+    , ((modMask .|. shiftMask, xK_Left ), sendMessage $ Swap L)
+    , ((modMask .|. shiftMask, xK_Up   ), sendMessage $ Swap U)
+    , ((modMask .|. shiftMask, xK_Down ), sendMessage $ Swap D)
+    
+    -- launch dmenu
+    , ((modMask,               xK_p     ), spawn "exe=`dmenu_path | dmenu -nb \\#500070 -nf white -sb white -sf black` && eval \"exec $exe\"")
 
   ]
 
