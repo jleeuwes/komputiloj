ad6bf2313f9593bf7405198395349eadc819e8e7
jeroen <jeroen@37c0c1ce-2331-4627-86c0-9f85f175e55b>
2014-12-15 22:22:06 +0000

    UI-verbeteringen. Ledger-upgrade.

--- settings/alderaan/.nix-defexpr/jeroen/default.nix
+++ settings/alderaan/.nix-defexpr/jeroen/default.nix
@@ -4,4 +4,13 @@ let
   nixpkgs = import <nixpkgs> {};
 in {
   dmenu = nixpkgs.dmenu.override { enableXft = true; };
+  
+  ledger = nixpkgs.lib.overrideDerivation nixpkgs.ledger3 (attrs: {
+    name = "ledger-3.1";
+    src = nixpkgs.fetchgit {
+      url = "git://github.com/ledger/ledger.git";
+      rev = "720c03b139";
+      sha256 = "1l5y4k830jyw7n1nnhssci3qahq091fj5cxcr77znk20nclz851s";
+    };
+  });
 }
