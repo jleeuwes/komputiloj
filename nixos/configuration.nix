1c95d0bc36f332099e0ad25e26dc686f1c0b36cb
root <root@scarif>
2017-02-19 22:20:12 +0100

    add file package

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -34,7 +34,7 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-    git vim
+    git vim file
   ];
 
   # List services that you want to enable:
