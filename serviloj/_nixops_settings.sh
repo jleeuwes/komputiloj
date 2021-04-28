b144ae7380a95a6dff77a2208e51061bc5316cc8
Jeroen Leeuwestein <jeroen@lwstn.eu>
2021-04-28 10:59:15 +0200

    serviloj command cleanup

--- serviloj/_nixops_settings.sh
+++ serviloj/_nixops_settings.sh
@@ -1,3 +1,4 @@
 # This version of nixops must be checked out and build using nix-build
 NIXOPS_CMD=~/code/nixops/result/bin/nixops
 export NIXOPS_STATE=~/komputiloj/serviloj/state.nixops
+export NIXOPS_DEPLOYMENT=serviloj
