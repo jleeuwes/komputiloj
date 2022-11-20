6a7afbfc8d9ef32d98a5d5d961dc22a0fb841729
Jeroen Leeuwestein <jeroen@lwstn.eu>
2022-11-20 11:35:06 +0100

    Fix hash mismatch for nextcloud apps

--- sources.d/nextcloud_25_apps/apps/calendar/default.nix
+++ sources.d/nextcloud_25_apps/apps/calendar/default.nix
@@ -3,5 +3,5 @@ fetchNextcloudApp {
   name    = "calendar";
   version = "4.1.0-rc.1";
   url     = "https://github.com/nextcloud-releases/calendar/releases/download/v4.1.0-rc1/calendar-v4.1.0-rc1.tar.gz";
-  sha256  = "1p97pbllv21cld06y3cqsld5ssk4a80g17fcw58i1d7g1m2i34a4";
+  sha256  = "1kpvdf7783bhify3ddmm7245xky8fakv6bdjhnv2j2f105nprfky";
 }
--- sources.d/nextcloud_25_apps/apps/files_linkeditor/default.nix
+++ sources.d/nextcloud_25_apps/apps/files_linkeditor/default.nix
@@ -3,5 +3,5 @@ fetchNextcloudApp {
   name    = "files_linkeditor";
   version = "1.1.12";
   url     = "https://github.com/te-online/nextcloud-app-releases/raw/main/files_linkeditor/v1.1.12/files_linkeditor.tar.gz";
-  sha256  = "044fgr92qxiin4qr4x3k3kp3qhvfz8g1k0iirh4d5f9y7b3ddxac";
+  sha256  = "1p3v7xdd9bds2b5qaphr46bncxh2fgcd1zicviyzkfl7ikvzjnsz";
 }
