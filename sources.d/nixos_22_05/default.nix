28c6ffb7489a0282d94651f3a97a914ddd049fb8
Jeroen Leeuwestein <jeroen@lwstn.eu>
2023-05-26 22:48:38 +0200

    Drop unused NixOS 22.05 source

--- sources.d/nixos_22_05/default.nix
+++
@@ -1,9 +0,0 @@
-# GENERATED
-with builtins;
-rec {
-    nix_path = fetchTarball {
-        url    = "https://releases.nixos.org/nixos/22.05/nixos-22.05.4690.08741686397/nixexprs.tar.xz";
-        sha256 = "09zikzglazai016hyjhby8dc0s4bld6s7yzb2x62a0kakbsp9414";
-    };
-    value = import nix_path;
-}
