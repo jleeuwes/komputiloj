8e600df5f9ccc453083b67301844c697eb640c56
Jeroen Leeuwestein <jeroen@lwstn.eu>
2023-06-07 12:58:45 +0200

    Reformat boltons.nix with spaces

--- lib/boltons.nix
+++ lib/boltons.nix
@@ -1,18 +1,18 @@
 # boltons: utilities and stuff missing from builtins.
 with builtins;
 builtins // rec {
-	# mapNames = (f: attrs:
-	# 	listToAttrs (map (key: {name = f key; value = getAttr key attrs;}) (attrNames attrs))
-	# );
+    # mapNames = (f: attrs:
+    #     listToAttrs (map (key: {name = f key; value = getAttr key attrs;}) (attrNames attrs))
+    # );
 
-	attrsToList = (attrs: map (key: {name = key; value = getAttr key attrs;}) (attrNames attrs));
-	
-	mapNames = (f: attrs: listToAttrs (map (a: {name = f a.name; value = a.value;}) (attrsToList attrs)));
+    attrsToList = (attrs: map (key: {name = key; value = getAttr key attrs;}) (attrNames attrs));
+    
+    mapNames = (f: attrs: listToAttrs (map (a: {name = f a.name; value = a.value;}) (attrsToList attrs)));
 
-	filterAttrs = (f: attrs: listToAttrs (filter f (attrsToList attrs)));
-	
-	dirnamesIn = dir: attrNames (filterAttrs (d: d.value == "directory") (readDir dir));
-	# deprecated snake case alias:
+    filterAttrs = (f: attrs: listToAttrs (filter f (attrsToList attrs)));
+    
+    dirnamesIn = dir: attrNames (filterAttrs (d: d.value == "directory") (readDir dir));
+    # deprecated snake case alias:
     dirnames_in = dirnamesIn;
 
     importDir = path: let
@@ -22,39 +22,39 @@ builtins // rec {
     in
         listToAttrs importedPairs;
 
-	lines = input:
-		filter (element: typeOf element == "string")
-		(split "\n" input);
-	
-	unlines = concatStringsSep "\n";
+    lines = input:
+        filter (element: typeOf element == "string")
+        (split "\n" input);
+    
+    unlines = concatStringsSep "\n";
 
-	mapLines = f: input: unlines (map f (lines input));
+    mapLines = f: input: unlines (map f (lines input));
 
-	replaceRegex = regex: substitution: input: let
-		splits = split regex input;
-		mapped = map (s: if typeOf s == "string" then s else substitution) splits;
-		in concatStringsSep "" mapped;
+    replaceRegex = regex: substitution: input: let
+        splits = split regex input;
+        mapped = map (s: if typeOf s == "string" then s else substitution) splits;
+        in concatStringsSep "" mapped;
 
-	charsToString = concatStringsSep "";
+    charsToString = concatStringsSep "";
 
-	# Untested, might be useful later
-	stringToChars = input: let
-		splits = split "(.)" input;
-		groups = filter (elem: typeOf elem == "list") splits;
-		in concatLists groups;
-	
-	# Untested, might be useful later
-	mapChars = f: input: let
-		chars = stringToChars input;
-		mappedChars = map f chars;
-		in charsToString mappedChars;
+    # Untested, might be useful later
+    stringToChars = input: let
+        splits = split "(.)" input;
+        groups = filter (elem: typeOf elem == "list") splits;
+        in concatLists groups;
+    
+    # Untested, might be useful later
+    mapChars = f: input: let
+        chars = stringToChars input;
+        mappedChars = map f chars;
+        in charsToString mappedChars;
 
-	# Determines the tab-based indentation of the first line of input,
-	# then strips that amount of tabs at te start of each line.
-	stripTabs = input: let
-		indentation = head (match "^(\t*).*$" input);
-		regex = "^" + indentation;
-		in mapLines (replaceRegex regex "") input;
-	
-	traceThisString = input: trace input input;
+    # Determines the tab-based indentation of the first line of input,
+    # then strips that amount of tabs at te start of each line.
+    stripTabs = input: let
+        indentation = head (match "^(\t*).*$" input);
+        regex = "^" + indentation;
+        in mapLines (replaceRegex regex "") input;
+    
+    traceThisString = input: trace input input;
 }
