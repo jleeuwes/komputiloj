66250e4439428e8a7e6129bd7a4b4efe9a4d8ced
Jeroen Leeuwestein <jeroen@lwstn.eu>
2023-08-23 15:59:56 +0200

    Workworkwork

--- apps/wolk/modules/nextcloud.nix
+++ apps/wolk/modules/nextcloud.nix
@@ -1,6 +1,172 @@
 { boltons, nextcloud, nixpkgsCurrent, ... }:
+with boltons;
 { ... }:
-{
+let
+    # TODO pull lib out of packages into the capsule?
+    escapeShellArg = nixpkgsCurrent.packages.lib.strings.escapeShellArg;
+    bigstorageService = rec {
+        serviceConfig = {
+            Type = "simple";
+            User = "nextcloud";
+            PermissionsStartOnly = true;
+        };
+        needsStorageVolume = "requires";
+        wants = [
+            "bigstorage1-wolk-webdav-pass-key.service"
+            "bigstorage1-wolk-crypt-pass-key.service"
+            "bigstorage1-wolk-crypt-salt-key.service"
+        ];
+        after = wants;
+        mailOnFailure = true;
+        path = [
+            nixpkgsCurrent.packages.rclone
+            nixpkgsCurrent.packages.gnugrep
+            nixpkgsCurrent.packages.coreutils
+            nixpkgsCurrent.packages.utillinux
+        ];
+        preStart = ''
+            # This script should run as root! Check PermissionsStartOnly.
+            
+            # To work around https://forum.rclone.org/t/detected-overridden-config/23371 and https://forum.rclone.org/t/different-cache-dir-suffixes-for-same-password/41114
+            # we generate a config file instead of using environment variables.
+            # This ExecPreStart runs as root so we can put the config on ramfs /run/keys
+            config_file=/run/keys/bigstorage1-wolk-config
+            umask u=rw,go=
+            touch -- "$config_file"
+            chown root:nextcloud -- "$config_file"
+            chmod g+r -- "$config_file"
+            cat > "$config_file" <<-EOF
+                [lower]
+                type=webdav
+                url=https://u362967-sub1.your-storagebox.de
+                vendor=other
+                user=u362967-sub1
+                pass=$(rclone obscure - < /run/keys/bigstorage1-wolk-webdav-pass)
+
+                [upper]
+                type=crypt
+                remote=lower:encrypted
+                password=$(rclone obscure - < /run/keys/bigstorage1-wolk-crypt-pass)
+                password2=$(rclone obscure - < /run/keys/bigstorage1-wolk-crypt-salt)
+            EOF
+        '';
+        script = ''
+            # ExecStart (this script) MUST NOT RUN AS ROOT!
+
+            # Give rclone access to fusermount3 wrapper
+            # (which hopefully is installed by default)
+            export PATH="$PATH":/run/wrappers/bin
+            
+            config_file=/run/keys/bigstorage1-wolk-config
+
+            rclone mount upper: /mnt/per-user/nextcloud/bigstorage \
+                --config="$config_file" \
+                --vfs-cache-mode=writes \
+                --vfs-cache-max-size=1G \
+                --cache-dir=/mnt/storage/work/nextcloud/vfs-cache
+        '';
+        postStart = ''
+            function is_mounted() {
+                # ExecStartPost runs as root, but root cannot inspect nextcloud's fuse mount,
+                # so we need to drop priviliges:
+                setpriv --reuid=nextcloud --regid=nextcloud --clear-groups mountpoint "$@"
+            }
+            
+            while kill -0 "$MAINPID"; do
+                if is_mounted /mnt/per-user/nextcloud/bigstorage; then
+                    echo "Bigstorage mounted."
+                    break
+                else
+                    echo "Waiting for bigstorage to mount..."
+                    sleep 1
+                fi
+            done
+        '';
+        postStop = ''
+            export PATH="$PATH":/run/wrappers/bin
+            fusermount -u /mnt/per-user/nextcloud/bigstorage
+        '';
+    };
+    bindmountService = user: rec {
+        serviceConfig = {
+            Type = "simple";
+            User = "nextcloud";
+        };
+        needsStorageVolume = "requires";
+        requires = [
+            "mount-nextcloud-bigstorage.service"
+        ];
+        after = requires;
+        mailOnFailure = true;
+        path = [
+            nixpkgsCurrent.packages.bindfs
+            nixpkgsCurrent.packages.coreutils
+            nixpkgsCurrent.packages.utillinux
+        ];
+        preStart = ''
+        '';
+        script = ''
+            user=${escapeShellArg user}
+            userdir=/mnt/storage/live/nextcloud/rootdir/data/"$user"
+            bigdir=/mnt/per-user/nextcloud/bigstorage/"$user"
+
+            function is_empty_dir() {
+                [[ -z "$(ls -A1q -- "$1")" ]]
+            }
+
+            if [[ ! -d "$userdir" ]]; then
+                printf '<3>Data dir %q does not exist so we can't mount there.\n' "$userdir"
+                exit 1
+            fi
+            mkdir -p  -- "$userdir"/files
+            if ! is_empty_dir "$userdir"/files)"; then
+                if is_empty_dir "$bigdir"; then
+                    printf '<3>There are files in %q that would disappear behind the bindmount. Aborting.\n' "$userdir"/files
+                    exit 1
+                else
+                    # The <4> makes it a warning: https://wiki.archlinux.org/title/Systemd/Journal
+                    printf '<4>There are files in both %q and %q! I'll continue but this should be investigated.\n' "$userdir"/files "$bigdir"
+                fi
+            fi
+            
+            # Dir must be writable to be able to mount
+            chmod u+w -- "$userdir"/files
+            # Slight window between chmod and bindfs in which files might be written :(
+            # Option -f makes bindfs run in the foreground
+            bindfs -f --no-allow-other -- "$bigdir" "$userdir"/files
+        '';
+        postStart = ''
+            user=${escapeShellArg user}
+            while kill -0 "$MAINPID"; do
+                if mountpoint -q /mnt/storage/live/nextcloud/rootdir/data/"$user"/files; then
+                    echo "Nextcloud user bindmounted: $user"
+                    break
+                else
+                    echo "Waiting for nextcloud user to mount: $user"
+                    sleep 1
+                fi
+            done
+        '';
+        postStop = ''
+            user=${escapeShellArg user}
+            userdir=/mnt/storage/live/nextcloud/rootdir/data/"$user"
+            export PATH="$PATH":/run/wrappers/bin
+
+            fusermount -u -- "$userdir"/files
+            # Try and prevent writing to the mountpoint when not mounted:
+            chmod ug-w -- "$userdir"/files
+            
+            function is_empty_dir() {
+                [[ -z "$(ls -A1q -- "$1")" ]]
+            }
+
+            if ! is_empty_dir "$userdir"/files)"; then
+                printf '<3>There are files in %q that would disappear behind the bindmount next time.\n' "$userdir"/files
+                exit 1 # attempt to trigger failure so we get a mail
+            fi
+        '';
+    };
+in {
     imports = [
         # TODO depends on storage-volume
     ];
@@ -61,109 +227,10 @@
         };
 
         systemd.services = {
-            mount-nextcloud-bigstorage = rec {
-                serviceConfig = {
-                    Type = "simple";
-                    User = "nextcloud";
-                };
-                needsStorageVolume = "requires";
-                wants = [
-                    "bigstorage1-wolk-webdav-pass-key.service"
-                    "bigstorage1-wolk-crypt-pass-key.service"
-                    "bigstorage1-wolk-crypt-salt-key.service"
-                ];
-                after = wants;
-                mailOnFailure = true;
-                path = [
-                    nixpkgsCurrent.packages.rclone
-                    nixpkgsCurrent.packages.gnugrep
-                    nixpkgsCurrent.packages.coreutils
-                    nixpkgsCurrent.packages.utillinux
-                ];
-                script = ''
-                    # Give rclone access to fusermount3 wrapper
-                    # (which hopefully is installed by default)
-                    export PATH="$PATH":/run/wrappers/bin
+            mount-nextcloud-bigstorage = bigstorageService;
 
-                    export RCLONE_CONFIG_LOWER_TYPE=webdav
-                    export RCLONE_CONFIG_LOWER_URL=https://u362967-sub1.your-storagebox.de
-                    export RCLONE_CONFIG_LOWER_VENDOR=other
-                    export RCLONE_CONFIG_LOWER_USER=u362967-sub1
-                    export RCLONE_CONFIG_LOWER_PASS=$(rclone obscure - < /run/keys/bigstorage1-wolk-webdav-pass)
-                    export RCLONE_CONFIG_UPPER_TYPE=crypt
-                    export RCLONE_CONFIG_UPPER_REMOTE=lower:encrypted
-                    export RCLONE_CONFIG_UPPER_PASSWORD=$(rclone obscure - < /run/keys/bigstorage1-wolk-crypt-pass)
-                    export RCLONE_CONFIG_UPPER_PASSWORD2=$(rclone obscure - < /run/keys/bigstorage1-wolk-crypt-salt)
-                    rclone mount upper: /mnt/per-user/nextcloud/bigstorage \
-                        --config=/dev/null \
-                        --vfs-cache-mode=writes \
-                        --vfs-cache-max-size=1G \
-                        --cache-dir=/mnt/storage/work/nextcloud/vfs-cache
-                '';
-                postStart = ''
-                    while kill -0 "$MAINPID"; do
-                        if mountpoint -q /mnt/per-user/nextcloud/bigstorage; then
-                            echo "Bigstorage mounted."
-                            break
-                        else
-                            echo "Waiting for bigstorage to mount..."
-                            sleep 1
-                        fi
-                    done
-                '';
-                preStop = ''
-                    export PATH="$PATH":/run/wrappers/bin
-                    fusermount -u /mnt/per-user/nextcloud/bigstorage
-                '';
-            };
-
-            mount-nextcloud-bindmounts = rec {
-                serviceConfig = {
-                    Type = "simple";
-                    User = "nextcloud";
-                };
-                needsStorageVolume = "requires";
-                requires = [
-                    "mount-nextcloud-bigstorage.service"
-                ];
-                after = requires;
-                mailOnFailure = true;
-                path = [
-                    nixpkgsCurrent.packages.bindfs
-                    nixpkgsCurrent.packages.coreutils
-                    nixpkgsCurrent.packages.utillinux
-                ];
-                preStart = ''
-                '';
-                script = ''
-                    test -d /mnt/storage/live/nextcloud/rootdir/data/testje
-                    mkdir -p /mnt/storage/live/nextcloud/rootdir/data/testje/files
-                    chmod u+w /mnt/storage/live/nextcloud/rootdir/data/testje/files
-                    # TODO warn if dir is not empty
-
-                    # Option -f makes bindfs run in the foreground
-                    bindfs -f --no-allow-other \
-                        /mnt/per-user/nextcloud/bigstorage/testje \
-                        /mnt/storage/live/nextcloud/rootdir/data/testje/files
-                '';
-                postStart = ''
-                    while kill -0 "$MAINPID"; do
-                        if mountpoint -q /mnt/storage/live/nextcloud/rootdir/data/testje/files; then
-                            echo "Nextcloud user testje bindmounted."
-                            break
-                        else
-                            echo "Waiting for nextcloud user testje to mount..."
-                            sleep 1
-                        fi
-                    done
-                '';
-                preStop = ''
-                    export PATH="$PATH":/run/wrappers/bin
-                    fusermount -u /mnt/storage/live/nextcloud/rootdir/data/testje/files
-                    # Try and prevent writing to the mountpoint when not mounted:
-                    chmod ug-w /mnt/storage/live/nextcloud/rootdir/data/testje/files
-                '';
-            };
+            mount-nextcloud-bindmount-testje = bindmountService "testje";
+            mount-nextcloud-bindmount-jeroen = bindmountService "jeroen";
 
             # Augment nextcloud's own services:
             # TODO requisite nextcloud-admin key (setup only?)
