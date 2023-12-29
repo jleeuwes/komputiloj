60ad2a69b80096720fbbb9103671b99ed5a14e35
Jeroen Leeuwestein <jeroen@lwstn.eu>
2023-12-29 17:23:14 +0100

    Clean up NixOS 23.05 sources

--- sources.d/nixos_23_05/default.nix
+++
@@ -1,9 +0,0 @@
-# GENERATED
-with builtins;
-rec {
-    nix_path = fetchTarball {
-        url    = "https://releases.nixos.org/nixos/23.05/nixos-23.05.5184.946310306972/nixexprs.tar.xz";
-        sha256 = "18wlrq100n210pv0b15c1xv68mxc85finn07ff61fliy14arfrqh";
-    };
-    value = import nix_path;
-}
