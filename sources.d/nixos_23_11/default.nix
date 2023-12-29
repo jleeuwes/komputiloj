5516ff6ed7d47ddc2cdb333634836796826d6dfa
Jeroen Leeuwestein <jeroen@lwstn.eu>
2023-12-29 17:23:36 +0100

    Updates

--- sources.d/nixos_23_11/default.nix
+++ sources.d/nixos_23_11/default.nix
@@ -2,8 +2,8 @@
 with builtins;
 rec {
     nix_path = fetchTarball {
-        url    = "https://releases.nixos.org/nixos/23.11/nixos-23.11.2171.0b3d61817311/nixexprs.tar.xz";
-        sha256 = "0wnxyfjbb8wbf9kw3z8ajxjwk0myani615272s9vyppz8vd6g4qv";
+        url    = "https://releases.nixos.org/nixos/23.11/nixos-23.11.2217.d02d818f22c7/nixexprs.tar.xz";
+        sha256 = "047zq3b8w0zqpx6dq064dsbbqly8sfyjxln6dlx0mzai8w1iaa3j";
     };
     value = import nix_path;
 }
