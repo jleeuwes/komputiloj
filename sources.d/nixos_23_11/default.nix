46da3f4fe8fdbff0abc29600097636df67a0253f
Jeroen Leeuwestein <jeroen@lwstn.eu>
2024-02-07 14:31:41 +0100

    Updates
    REGRESSION: bluetooth mouse no longer works :(

--- sources.d/nixos_23_11/default.nix
+++ sources.d/nixos_23_11/default.nix
@@ -2,8 +2,8 @@
 with builtins;
 rec {
     nix_path = fetchTarball {
-        url    = "https://releases.nixos.org/nixos/23.11/nixos-23.11.3684.f4a8d6d5324c/nixexprs.tar.xz";
-        sha256 = "10bkzz6isjf18wcma02kqkjnxz23xl6n0nvdnawxi6sxw6wdighq";
+        url    = "https://releases.nixos.org/nixos/23.11/nixos-23.11.4030.9f2ee8c91ac4/nixexprs.tar.xz";
+        sha256 = "1ngsfxmhvm6zp1wzjl1z69q9hv4l2kbm99dbhgfjr4py9wpiwwsk";
     };
     value = import nix_path;
 }
