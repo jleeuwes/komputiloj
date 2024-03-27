32922634086d077e69fcefc28edbe16b094bb2d2
Jeroen Leeuwestein <jeroen@lwstn.eu>
2024-03-27 16:25:33 +0100

    Remove nixops from our workflow
    Note that we do still use nixops-like key management,
    so nixops might still show up in some names here and there.

--- sources.d/nixops/default.nix
+++
@@ -1,9 +0,0 @@
-# GENERATED
-with builtins;
-rec {
-    nix_path = fetchTarball {
-        url    = "https://github.com/NixOS/nixops/archive/45256745cef246dabe1ae8a7d109988f190cd7ef.tar.gz";
-        sha256 = "0ni1v8ppg5cf35gq7nzd50kajxzp5zkbzhf022in0fgbjcprlzr2";
-    };
-    value = import nix_path;
-}
