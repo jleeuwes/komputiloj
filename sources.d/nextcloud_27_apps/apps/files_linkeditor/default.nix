84fd8084953680c63f3604117dd14c5498937292
Jeroen Leeuwestein <jeroen@lwstn.eu>
2024-04-10 15:36:34 +0200

    Updates

--- sources.d/nextcloud_27_apps/apps/files_linkeditor/default.nix
+++ sources.d/nextcloud_27_apps/apps/files_linkeditor/default.nix
@@ -2,8 +2,8 @@
 {fetchNextcloudApp}:
 fetchNextcloudApp {
   # name    = "files_linkeditor";
-  # version = "1.1.19";
-  url     = "https://github.com/te-online/nextcloud-app-releases/raw/main/files_linkeditor/v1.1.19/files_linkeditor.tar.gz";
-  sha256  = "1bl2074zxbfvfr09y5alvvcrvl1vcjmhg9q7f3sbpjb6rpmfarjy";
+  # version = "1.1.20";
+  url     = "https://github.com/te-online/nextcloud-app-releases/raw/main/files_linkeditor/v1.1.20/files_linkeditor.tar.gz";
+  sha256  = "08j2l6kajmsrf6f54i1ailrk63m1pjnvpwr49ca40qagwxjvjrdn";
   license = "agpl3Plus";
 }
--- sources.d/nixos_23_11/default.nix
+++ sources.d/nixos_23_11/default.nix
@@ -2,8 +2,8 @@
 with builtins;
 rec {
     nix_path = fetchTarball {
-        url    = "https://releases.nixos.org/nixos/23.11/nixos-23.11.5742.219951b495fc/nixexprs.tar.xz";
-        sha256 = "0710zalhzcfw725lj8rryx2vhj1mjcxllvxiz53pdqq4l6rw63vn";
+        url    = "https://releases.nixos.org/nixos/23.11/nixos-23.11.5987.d272ca50d1f7/nixexprs.tar.xz";
+        sha256 = "1ccxi3sdlrknnpvjq87whvyz3pqgmvqwsihbmqhw38i0v20w9y78";
     };
     value = import nix_path;
 }
