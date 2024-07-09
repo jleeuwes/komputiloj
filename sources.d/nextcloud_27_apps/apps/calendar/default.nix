534c82f8857d069fabcf207cfa5b05a0f65d1a86
Jeroen Leeuwestein <jeroen@lwstn.eu>
2024-07-09 21:30:28 +0200

    Updates

--- sources.d/nextcloud_27_apps/apps/calendar/default.nix
+++ sources.d/nextcloud_27_apps/apps/calendar/default.nix
@@ -2,8 +2,8 @@
 {fetchNextcloudApp}:
 fetchNextcloudApp {
   # name    = "calendar";
-  # version = "4.7.6";
-  url     = "https://github.com/nextcloud-releases/calendar/releases/download/v4.7.6/calendar-v4.7.6.tar.gz";
-  sha256  = "09rsp5anpaqzwmrixza5qh12vmq9hd3an045064vm3rnynz537qc";
+  # version = "4.7.9";
+  url     = "https://github.com/nextcloud-releases/calendar/releases/download/v4.7.9/calendar-v4.7.9.tar.gz";
+  sha256  = "1yv1cb4lnp59s37f8h4lx4naqfdqddcdcikc56cx6p30n0pnd4k4";
   license = "agpl3Plus";
 }
--- sources.d/nixos_24_05/default.nix
+++ sources.d/nixos_24_05/default.nix
@@ -2,8 +2,8 @@
 with builtins;
 rec {
     nix_path = fetchTarball {
-        url    = "https://releases.nixos.org/nixos/24.05/nixos-24.05.1695.dd457de7e08c/nixexprs.tar.xz";
-        sha256 = "15gnmmcyrb5fyhs8f0q5b7abxjla9i54zy7l6b3pvdazh9k3qf2y";
+        url    = "https://releases.nixos.org/nixos/24.05/nixos-24.05.2580.194846768975/nixexprs.tar.xz";
+        sha256 = "1qzpw7s6sms6z1i8nch28ncl9lg8xk901afw5601iq5jhcaprs5p";
     };
     value = import nix_path;
 }
