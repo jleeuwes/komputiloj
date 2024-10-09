f0ba096734be981da63bcb5c25ba45984aad1570
Jeroen Leeuwestein <jeroen@lwstn.eu>
2024-10-09 14:30:08 +0200

    Add ferrix key to gitea

--- users.d/jeroen/default.nix
+++ users.d/jeroen/default.nix
@@ -1,5 +1,5 @@
 with builtins;
-{
+rec {
     name = "jeroen";
     fullName = "Jeroen Leeuwestein";
     apps.thee = {
@@ -12,8 +12,16 @@ with builtins;
     linux.uid = 1000;
     email = "jeroen@lwstn.eu";
     isHuman = true;
+    
+    # TODO replace usages of this:
     sshKeys = {
         # TODO place the key besides this nix file? But then where do we put the private key?
         scarif = readFile ../../machines.d/scarif/home/jeroen/.ssh/id_rsa.pub;
+        ferrix = ''
+            ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDWHWQVPZcUu5B1CAaobojr8VHa3/MZQhxRHrPWQIZrq
+        '';
     };
+
+    # with this (used by thee):
+    ssh.publicKeys = sshKeys;
 }
