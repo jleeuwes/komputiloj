5f7295614f471af403676050cdad2e7d415a284d
Jeroen Leeuwestein <jeroen@lwstn.eu>
2024-10-16 20:26:07 +0200

    Enable ssh agent

--- machines.d/ferrix/default.nix
+++ machines.d/ferrix/default.nix
@@ -278,6 +278,10 @@ rec {
                   publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
                 };
             };
+            startAgent = true;
+            extraConfig = ''
+                AddKeysToAgent confirm
+            '';
         };
 
         # Enable CUPS and printing.
