141e07e6973f2ea82ac4c83dc0707cacaed476e9
Jeroen Leeuwestein <jeroen@lwstn.eu>
2024-12-21 16:18:39 +0100

    Autologin and no lock screen

--- machines.d/ferrix/default.nix
+++ machines.d/ferrix/default.nix
@@ -391,7 +391,16 @@ rec {
         # You can disable this if you're only using the Wayland session.
         services.xserver.enable = true;
         # Enable the KDE Plasma Desktop Environment.
-        services.displayManager.sddm.enable = true;
+        services.displayManager = {
+            sddm = {
+                enable = true;
+                autoLogin.relogin = true;
+            };
+            autoLogin = {
+                enable = true;
+                user = "jeroen";
+            };
+        };
         services.desktopManager.plasma6.enable = true;
         services.xserver.xkb = {
           layout = "us";
