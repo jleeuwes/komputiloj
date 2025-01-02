5fd8b4878924a65312cba240f3fc82c437acf86b
Jeroen Leeuwestein <jeroen@lwstn.eu>
2025-01-02 21:47:36 +0100

    Updates

--- sources.d/mailserver_24_05/default.nix
+++ sources.d/mailserver_24_05/default.nix
@@ -2,8 +2,8 @@
 with builtins;
 rec {
     nix_path = fetchTarball {
-        url    = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/29916981e7b3b5782dc5085ad18490113f8ff63b/nixos-mailserver-29916981e7b3b5782dc5085ad18490113f8ff63b.tar.gz";
-        sha256 = "0clvw4622mqzk1aqw1qn6shl9pai097q62mq1ibzscnjayhp278b";
+        url    = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/636b82f4175e3f6b1e80d2189bb0469e2ae01a55/nixos-mailserver-636b82f4175e3f6b1e80d2189bb0469e2ae01a55.tar.gz";
+        sha256 = "08zdidja5kdqgskynxsmcd8skh1b7cfl9ijjy9pak4b5h3aw2iqv";
     };
     value = import nix_path;
 }
--- sources.d/nixos_24_05/default.nix
+++ sources.d/nixos_24_05/default.nix
@@ -2,8 +2,8 @@
 with builtins;
 rec {
     nix_path = fetchTarball {
-        url    = "https://releases.nixos.org/nixos/24.05/nixos-24.05.6463.83fb6c028368/nixexprs.tar.xz";
-        sha256 = "1h73z8c0fp8sh9wvh80gyv99hncnzzdrah65lyb5f47z3w2hqiqc";
+        url    = "https://releases.nixos.org/nixos/24.05/nixos-24.05.7376.b134951a4c9f/nixexprs.tar.xz";
+        sha256 = "1f8j7fh0nl4qmqlxn6lis8zf7dnckm6jri4rwmj0qm1qivhr58lv";
     };
     value = import nix_path;
 }
