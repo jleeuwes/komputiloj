aacb3f52ad641d50c4b9646a7df7c80320949c86
Jeroen Leeuwestein <jeroen@lwstn.eu>
2025-01-24 11:08:00 +0100

    Fix hash mismatches for nextcloud apps

--- sources.d/nextcloud_29_apps/apps/calendar/default.nix
+++ sources.d/nextcloud_29_apps/apps/calendar/default.nix
@@ -6,4 +6,5 @@ fetchNextcloudApp {
   url     = "https://github.com/nextcloud-releases/calendar/releases/download/v4.7.9/calendar-v4.7.9.tar.gz";
   sha256  = "1yv1cb4lnp59s37f8h4lx4naqfdqddcdcikc56cx6p30n0pnd4k4";
   license = "agpl3Plus";
+  unpack = true;
 }
--- sources.d/nextcloud_29_apps/apps/files_linkeditor/default.nix
+++ sources.d/nextcloud_29_apps/apps/files_linkeditor/default.nix
@@ -6,4 +6,5 @@ fetchNextcloudApp {
   url     = "https://github.com/te-online/nextcloud-app-releases/raw/main/files_linkeditor/v1.1.21/files_linkeditor.tar.gz";
   sha256  = "1zvnrwacffk6p5akqhpd9azaap56b88z9hirqa3xvbwm56kii4xz";
   license = "agpl3Plus";
+  unpack = true;
 }
