a9ccaed0b3dfaba3407acb1f689448f6d724b3a9
Jeroen Leeuwestein <jeroen@lwstn.eu>
2025-02-08 20:44:05 +0100

    Update hello-infra and use our own fork of raspberry-pi-nix

--- sources.d/raspberry-pi-nix/default.nix
+++ sources.d/raspberry-pi-nix/default.nix
@@ -2,8 +2,8 @@
 with builtins;
 rec {
     nix_path = fetchTarball {
-        url    = "https://github.com/nix-community/raspberry-pi-nix/archive/628e512d60fa99f8f49e73e39b7cedf9b968c282.tar.gz";
-        sha256 = "11hp809ifviwjjfrz401m9j31kz7hrj553afwwfw3ywa0kcvxkxp";
+        url    = "https://github.com/jleeuwes/raspberry-pi-nix/archive/824ce252585a8fbf8b50023da67c8405d5f4ff85.tar.gz";
+        sha256 = "0r3hlcxikfjd4pbbalznsd0jr8ygj5idvysh6206vyq0pyjzhq6j";
     };
     value = import nix_path;
 }
