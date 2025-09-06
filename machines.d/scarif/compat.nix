6f9727ff18442aa818ff32c19b707064baf1d7b2
Jeroen Leeuwestein <jeroen@lwstn.eu>
2025-09-06 14:57:32 +0200

    I don't think this is needed anymore

--- machines.d/scarif/compat.nix
+++
@@ -1,7 +0,0 @@
-# compatibility with nixos-rebuild
-let
-    topLevel = import ../..;
-    capsules = topLevel.capsules;
-    nixpkgsFuture = capsules.nixpkgsFuture;
-    komputiloj = capsules.komputiloj;
-in ((import ./.) capsules).mainModule
