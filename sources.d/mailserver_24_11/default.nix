9d7bee868e8a49d83c91a4ea1b84793987a45fae
Jeroen Leeuwestein <jeroen@lwstn.eu>
2025-09-17 18:23:09 +0200

    Remove old sources

--- sources.d/mailserver_24_11/default.nix
+++
@@ -1,9 +0,0 @@
-# GENERATED
-with builtins;
-rec {
-    nix_path = fetchTarball {
-        url    = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/14857a0309d7bbdf7c51bbfa309d0d13448ae77e/nixos-mailserver-14857a0309d7bbdf7c51bbfa309d0d13448ae77e.tar.gz";
-        sha256 = "0lgrqdgb4z45ng875pa47m2vm7p3hhxn4n80x9z4qwvcdrrxrgch";
-    };
-    value = import nix_path;
-}
--- sources.d/nextcloud_30_apps/default.nix
+++
@@ -1,9 +0,0 @@
-# GENERATED
-{
-  value = pkgs:
-  let fetchNextcloudApp = pkgs.fetchNextcloudApp;
-  in {
-    calendar = import ./apps/calendar { inherit fetchNextcloudApp; };
-    files_linkeditor = import ./apps/files_linkeditor { inherit fetchNextcloudApp; };
-  };
-}
