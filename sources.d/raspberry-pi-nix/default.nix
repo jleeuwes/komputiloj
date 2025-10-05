d9d05ac2cd87508109a791e7e68b78eb3675af2d
Jeroen Leeuwestein <jeroen@lwstn.eu>
2025-10-05 13:04:32 +0200

    Theoretically switch raspberry-pi-nix to newest master

--- sources.d/raspberry-pi-nix/default.nix
+++ sources.d/raspberry-pi-nix/default.nix
@@ -2,8 +2,8 @@
 with builtins;
 rec {
     nix_path = fetchTarball {
-        url    = "https://github.com/jleeuwes/raspberry-pi-nix/archive/824ce252585a8fbf8b50023da67c8405d5f4ff85.tar.gz";
-        sha256 = "0r3hlcxikfjd4pbbalznsd0jr8ygj5idvysh6206vyq0pyjzhq6j";
+        url    = "https://github.com/nix-community/raspberry-pi-nix/archive/3e8100d5e976a6a2be363015cb33463af9ef441a.tar.gz";
+        sha256 = "0hcv8z6kjs47sy7lhpwmjih6f2k2jkkfg7wsprirv533pvrg7m34";
     };
     value = import nix_path;
 }
