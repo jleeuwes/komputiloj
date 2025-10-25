df1f94a26ae93a8f6b2cf06658a8d9e3f5b44659
Jeroen Leeuwestein <jeroen@lwstn.eu>
2025-10-25 15:11:17 +0200

    Update hello-infra and get rid of raspberry-pi-nix source

--- sources.d/raspberry-pi-nix/default.nix
+++
@@ -1,9 +0,0 @@
-# GENERATED
-with builtins;
-rec {
-    nix_path = fetchTarball {
-        url    = "https://github.com/nix-community/raspberry-pi-nix/archive/3e8100d5e976a6a2be363015cb33463af9ef441a.tar.gz";
-        sha256 = "0hcv8z6kjs47sy7lhpwmjih6f2k2jkkfg7wsprirv533pvrg7m34";
-    };
-    value = import nix_path;
-}
