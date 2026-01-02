c5e6c90e2388eb4916c605ba993cc4b4ba81d7fd
Jeroen Leeuwestein <jeroen@lwstn.eu>
2026-01-02 12:18:26 +0100

    Updates

--- capsules/nixos_25_05/pins.d/nixpkgs/lock.nix
+++ capsules/nixos_25_05/pins.d/nixpkgs/lock.nix
@@ -1,8 +1,8 @@
 # GENERATED
 with builtins;
 rec {
-    url = "https://releases.nixos.org/nixos/25.05/nixos-25.05.813731.a6c3a6141ec1/nixexprs.tar.xz";
-    sha256 = "0dpxylw6qn75hy4x348x70wa8dwlk5xb4m7nfr6yf8qjdz7bmv2x";
+    url = "https://releases.nixos.org/nixos/25.05/nixos-25.05.813798.40ee5e1944be/nixexprs.tar.xz";
+    sha256 = "0mcs3sp0nw1dc73h5xhnyrscizixkcp695g29ax73ls4ij38d4vx";
     nix_path = fetchTarball {
         inherit url sha256;
     };
--- capsules/nixos_25_11/pins.d/nixpkgs/lock.nix
+++ capsules/nixos_25_11/pins.d/nixpkgs/lock.nix
@@ -1,8 +1,8 @@
 # GENERATED
 with builtins;
 rec {
-    url = "https://releases.nixos.org/nixos/25.11/nixos-25.11.2403.76701a179d3a/nixexprs.tar.xz";
-    sha256 = "0d1nbibrj8sd13v4vvdr4b6vw9bf3wqzilfnlalf6mhcv0jc9y22";
+    url = "https://releases.nixos.org/nixos/25.11/nixos-25.11.2793.89dbf01df72e/nixexprs.tar.xz";
+    sha256 = "0lkphz9j38s7dx008hl6zbcn26hha7ml8kr962dkgcpizlwshc54";
     nix_path = fetchTarball {
         inherit url sha256;
     };
