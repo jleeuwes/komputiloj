df34f5e01b5146dffa287f113bd6988cc93b67ca
Jeroen Leeuwestein <jeroen@lwstn.eu>
2026-01-04 21:08:18 +0100

    Update nixos_25_11

--- capsules/nixos_25_11/pins.d/nixpkgs/lock.nix
+++ capsules/nixos_25_11/pins.d/nixpkgs/lock.nix
@@ -1,8 +1,8 @@
 # GENERATED
 with builtins;
 rec {
-    url = "https://releases.nixos.org/nixos/25.11/nixos-25.11.2793.89dbf01df72e/nixexprs.tar.xz";
-    sha256 = "0lkphz9j38s7dx008hl6zbcn26hha7ml8kr962dkgcpizlwshc54";
+    url = "https://releases.nixos.org/nixos/25.11/nixos-25.11.3202.30a3c519afcf/nixexprs.tar.xz";
+    sha256 = "1gsa22ibk3byzdlvav38gqzkyi8q4kcvv3a7a18y30p7aqxk6n8h";
     nix_path = fetchTarball {
         inherit url sha256;
     };
