963899efe03770ad311e41030f3f9eae795c368d
Jeroen Leeuwestein <jeroen@lwstn.eu>
2026-05-27 21:18:57 +0200

    Disable sending masterkey on deploy

--- commands.d/deploy-to-gently.nix
+++ commands.d/deploy-to-gently.nix
@@ -15,7 +15,7 @@ in command-platform.local.packageBuilders.writeCommand {
             --to ${esc sshTarget} \
             --gzip --use-substitutes \
             "$new_toplevel"
-        ${komputiloj.commands.send-masterkey-to-gently}
+        # ${komputiloj.commands.send-masterkey-to-gently}
         # We assume nix-env is present on the remote machine.
         ${sshCmd} nix-env -p /nix/var/nix/profiles/system --set "$new_toplevel"
         ${sshCmd} "$new_toplevel"/bin/switch-to-configuration switch
